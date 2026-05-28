606cc3d85e0eeebb0221b319c9ea9703432c0ce5
arjun8078 <arjunkanhangad143@gmail.com>
2026-05-28 14:27:54 +0530

    API endpoint changed

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use('/api/restaurants', restaurantRoutes)
+app.use('/restaurants', restaurantRoutes)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hungryfy API running' })
