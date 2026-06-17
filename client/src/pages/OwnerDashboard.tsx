644a41636e053ef6e36c5d1abf7215eb66fa693f
arjun8078 <arjunkanhangad143@gmail.com>
2026-06-17 16:53:47 +0530

    Setup owner dashbard with useReducer

--- client/src/pages/OwnerDashboard.tsx
+++ client/src/pages/OwnerDashboard.tsx
@@ -1,3 +1,21 @@
 export function OwnerDashboard() {
+
+ const initialState={
+  name:'',
+  cuisine:'',
+  area:'',
+  isVeg:false,
+  long:'',
+  lat:'',
+  description:'',
+  openingHour:'',
+  phone:''
+ }
+
+ type State=typeof initialState
+ type Action=
+              |  {type:"UPDATE_FIELD";field:keyof State;value:string}
+              |  { type: 'RESET' }
+
   return <div>Owner Dashboard (coming soon)</div>
 }
